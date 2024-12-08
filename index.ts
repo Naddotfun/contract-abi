c3cacb7066088aa447bf09964a335d5b56fcc209
minwoo <100527953+minwoogramer@users.noreply.github.com>
2024-12-08 20:20:22 +0900

    feat : add abi export

---
+++ index.ts
@@ -0,0 +1,7 @@
+export {default as IBondingCurve} from "./IBondingCurve.json"
+export {default as IBondingCurveFactory} from "./IBondingCurveFactory.json"
+export {default as ICore} from "./ICore.json"
+export {default as IMintParty} from "./IMintParty.json"
+export {default as IMintPartyFactory} from "./IMintPartyFactory.json"
+export {default as IToken} from "./IToken.json"
+
